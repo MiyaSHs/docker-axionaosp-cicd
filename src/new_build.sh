58e16b07deda5155aaee587b1b7ef2e2d916995d
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2025-12-24 18:44:14 -0300

    Update new_build.sh

--- src/new_build.sh
+++ src/new_build.sh
@@ -2,7 +2,7 @@
 
 # Slimmed-down Docker build script
 # Copyright (c) 2017 Julian Xhokaxhiu
-# Copyright (C) 2017-2018 Nicola Corna <nicola@corna.info>
+# Copyright (c) 2017-2018 Nicola Corna <nicola@corna.info>
 # Copyright (C) 2024 Pete Fotheringham <petefoth@e.email>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -114,12 +114,13 @@ fi
 
 # Handle parameters and environment variables
 branch=$BRANCH_NAME
+manifest_url=${MANIFEST_URL:-https://github.com/AxionAOSP/android.git}
 echo ">> [$(date)] Branch:  $branch"
 
 devices=$DEVICE_LIST
 echo ">> [$(date)] Devices: $devices"
 
-vendor=lineage
+vendor=${ROM_VENDOR:-axion}
 
 if [ "$BUILD_OVERLAY" = true ]; then
   echo "Using BUILD_OVERLAY is not yet working"
@@ -154,6 +155,13 @@ if [ -n "${RETRY_FETCHES-}" ]; then
   fi
 fi
 
+# repo init extra args (space-separated, forwarded as-is)
+repo_init_extra_args=()
+if [ -n "${REPO_INIT_ARGS-}" ]; then
+  # shellcheck disable=SC2206
+  repo_init_extra_args+=( $REPO_INIT_ARGS )
+fi
+
 # Branch-specific stuff
 branch_dir=${branch//[^[:alnum:]]/_}
 branch_dir=${branch_dir^^}
@@ -172,7 +180,7 @@ android_version_major=$(echo "($branch_num - 7) / 1" | bc)
 if [ "$RESET_VENDOR_UNDO_PATCHES" = true ]; then
   # Remove previous changes of vendor/cm, vendor/lineage and frameworks/base (if they exist)
   # TODO: maybe reset everything using https://source.android.com/setup/develop/repo#forall
-  for path in "vendor/cm" "vendor/lineage" "frameworks/base" "packages/apps/PermissionController" "packages/modules/Permission"; do
+  for path in "vendor/cm" "vendor/lineage" "vendor/axion" "frameworks/base" "packages/apps/PermissionController" "packages/modules/Permission"; do
     if [ -d "$path" ]; then
       cd "$path"
       git reset -q --hard
@@ -246,9 +254,9 @@ for codename in ${devices//,/ }; do
     if [ "$CALL_REPO_INIT" = true ]; then
       echo ">> [$(date)] (Re)initializing branch repository" | tee -a "$repo_log"
       if [ "$LOCAL_MIRROR" = true ]; then
-        ( yes||: ) | repo init -u https://github.com/LineageOS/android.git --reference "$MIRROR_DIR" -b "$branch" -g default,-darwin,-muppets,muppets_"${codename}" --git-lfs &>> "$repo_log"
+        ( yes||: ) | repo init -u "$manifest_url" --reference "$MIRROR_DIR" -b "$branch" "${repo_init_extra_args[@]}" --git-lfs &>> "$repo_log"
       else
-        ( yes||: ) | repo init -u https://github.com/LineageOS/android.git -b "$branch" -g default,-darwin,-muppets,muppets_"${codename}" --git-lfs &>> "$repo_log"
+        ( yes||: ) | repo init -u "$manifest_url" -b "$branch" "${repo_init_extra_args[@]}" --git-lfs &>> "$repo_log"
       fi
     else
       echo ">> [$(date)] Calling repo init disabled"
@@ -291,6 +299,12 @@ for codename in ${devices//,/ }; do
       echo ">> [$(date)] Calling git lfs pull disabled" | tee -a "$repo_log"
     fi
 
+    # Determine vendor tree (Axion uses vendor/axion; some derivatives still use vendor/lineage)
+    if [ ! -d "vendor/$vendor" ] && [ -d "vendor/lineage" ]; then
+      echo ">> [$(date)] vendor/$vendor not found; falling back to vendor/lineage" | tee -a "$repo_log"
+      vendor="lineage"
+    fi
+
     if [ ! -d "vendor/$vendor" ]; then
       echo ">> [$(date)] Missing \"vendor/$vendor\", aborting"
       exit 1
@@ -300,20 +314,19 @@ for codename in ${devices//,/ }; do
     if [ "$BUILD_OVERLAY" = true ]; then
       echo "Using BUILD_OVERLAY is not yet working"
       exit 1
-
-      # lowerdir=$SRC_DIR/$branch_dir
-      # upperdir=$TMP_DIR/device
-      # workdir=$TMP_DIR/workdir
-      # merged=$TMP_DIR/merged
-      # mkdir -p "$upperdir" "$workdir" "$merged"
-      # mount -t overlay overlay -o lowerdir="$lowerdir",upperdir="$upperdir",workdir="$workdir" "$merged"
-      # source_dir="$TMP_DIR/merged"
     else
       source_dir="$SRC_DIR/$branch_dir"
     fi
 
-    mkdir -p "vendor/$vendor/overlay/microg/"
-    sed -i "1s;^;PRODUCT_PACKAGE_OVERLAYS := vendor/$vendor/overlay/microg\n;" "vendor/$vendor/config/common.mk"
+    overlay_dir="vendor/$vendor/overlay/custom"
+    common_mk="vendor/$vendor/config/common.mk"
+    mkdir -p "$overlay_dir"
+    if [ -f "$common_mk" ]; then
+      overlay_line="PRODUCT_PACKAGE_OVERLAYS := $overlay_dir"
+      if ! grep -qF "$overlay_line" "$common_mk"; then
+        sed -i "1s;^;$overlay_line\n;" "$common_mk"
+      fi
+    fi
 
     makefile_containing_version="vendor/$vendor/config/common.mk"
     if [ -f "vendor/$vendor/config/version.mk" ]; then
@@ -323,8 +336,6 @@ for codename in ${devices//,/ }; do
     los_ver_minor=$(sed -n -e 's/^\s*PRODUCT_VERSION_MINOR = //p' "$makefile_containing_version")
     los_ver="$los_ver_major.$los_ver_minor"
 
-#    DEBUG_LOG="$LOGS_DIR/$logsubdir/lineage-$los_ver-$builddate-$RELEASE_TYPE-$codename.log"
-
     # Set RELEASE_TYPE
     echo ">> [$(date)] Setting \"$RELEASE_TYPE\" as release type"
     sed -i "/\$(filter .*\$(${vendor^^}_BUILDTYPE)/,/endif/d" "$makefile_containing_version"
@@ -333,24 +344,20 @@ for codename in ${devices//,/ }; do
     echo ">> [$(date)] Adding OTA URL overlay (for custom URL $OTA_URL)"
     if [ -n "$OTA_URL" ]; then
       if [ -d "packages/apps/Updater/app/src/main/res/values" ]; then
-        # "New" Updater project structure
         updater_values_dir="packages/apps/Updater/app/src/main/res/values"
       elif [ -d "packages/apps/Updater/res/values" ]; then
-        # "Old" Updater project structure
         updater_values_dir="packages/apps/Updater/res/values"
       else
         echo ">> [$(date)] ERROR: no 'values' dir of Updater app found"
         exit 1
       fi
 
-      updater_url_overlay_dir="vendor/$vendor/overlay/microg/${updater_values_dir}/"
+      updater_url_overlay_dir="${overlay_dir}/${updater_values_dir}/"
       mkdir -p "$updater_url_overlay_dir"
 
       if grep -q updater_server_url ${updater_values_dir}/strings.xml; then
-        # "New" updater configuration: full URL (with placeholders {device}, {type} and {incr})
         sed "s|{name}|updater_server_url|g; s|{url}|$OTA_URL/v1/{device}/{type}/{incr}|g" /root/packages_updater_strings.xml > "$updater_url_overlay_dir/strings.xml"
       elif grep -q conf_update_server_url_def ${updater_values_dir}/strings.xml; then
-        # "Old" updater configuration: just the URL
         sed "s|{name}|conf_update_server_url_def|g; s|{url}|$OTA_URL|g" /root/packages_updater_strings.xml > "$updater_url_overlay_dir/strings.xml"
       else
         echo ">> [$(date)] ERROR: no known Updater URL property found"
@@ -361,20 +368,31 @@ for codename in ${devices//,/ }; do
     # Add custom packages to be installed
     if [ -n "$CUSTOM_PACKAGES" ]; then
       echo ">> [$(date)] Adding custom packages ($CUSTOM_PACKAGES)"
-      sed -i "1s;^;PRODUCT_PACKAGES += $CUSTOM_PACKAGES\n\n;" "vendor/$vendor/config/common.mk"
+      if [ -f "$common_mk" ]; then
+        sed -i "1s;^;PRODUCT_PACKAGES += $CUSTOM_PACKAGES\n\n;" "$common_mk"
+      else
+        echo ">> [$(date)] WARNING: $common_mk not found; cannot add CUSTOM_PACKAGES" | tee -a "$DEBUG_LOG"
+      fi
     fi
 
     # Handle keys
     if [ "$SIGN_BUILDS" = true ]; then
       echo ">> [$(date)] Adding keys path ($KEYS_DIR)"
-      # Soong (Android 9+) complains if the signing keys are outside the build path
       ln -sf "$KEYS_DIR" user-keys
       if [ "$android_version_major" -lt "10" ]; then
-        sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey\nPRODUCT_EXTRA_RECOVERY_KEYS := user-keys/releasekey\n\n;" "vendor/$vendor/config/common.mk"
+        if [ -f "$common_mk" ]; then
+          sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey\nPRODUCT_EXTRA_RECOVERY_KEYS := user-keys/releasekey\n\n;" "$common_mk"
+        else
+          echo ">> [$(date)] WARNING: $common_mk not found; cannot set signing keys in common.mk" | tee -a "$DEBUG_LOG"
+        fi
       fi
 
       if [ "$android_version_major" -ge "10" ]; then
-        sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey\n\n;" "vendor/$vendor/config/common.mk"
+        if [ -f "$common_mk" ]; then
+          sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey\n\n;" "$common_mk"
+        else
+          echo ">> [$(date)] WARNING: $common_mk not found; cannot set signing keys in common.mk" | tee -a "$DEBUG_LOG"
+        fi
       fi
     fi
 
@@ -399,20 +417,32 @@ for codename in ${devices//,/ }; do
       /root/userscripts/before.sh "$branch" || { echo ">> [$(date)] Error: before.sh failed for $branch!"; userscriptfail=true; continue; }
     fi
 
-    # Call breakfast
-    breakfast_returncode=0
+    # Lunch / configure build target (Axion provides `axion`; fall back to Lineage `breakfast`)
+    lunch_returncode=0
     if [ "$CALL_BREAKFAST" = true ]; then
       set +eu
-      breakfast "$codename" "$BUILD_TYPE" &>> "$DEBUG_LOG"
-      breakfast_returncode=$?
+      if declare -F axion >/dev/null 2>&1; then
+        if [ -n "${AXION_VARIANT-}" ]; then
+          # shellcheck disable=SC2086
+          axion "$codename" $AXION_VARIANT &>> "$DEBUG_LOG"
+        else
+          if [ "$WITH_GMS" = true ]; then
+            axion "$codename" gms "${AXION_GMS_VARIANT:-core}" &>> "$DEBUG_LOG"
+          else
+            axion "$codename" va &>> "$DEBUG_LOG"
+          fi
+        fi
+      else
+        breakfast "$codename" "$BUILD_TYPE" &>> "$DEBUG_LOG"
+      fi
+      lunch_returncode=$?
       set -eu
     else
-      echo ">> [$(date)] Calling breakfast disabled"
+      echo ">> [$(date)] Lunch step disabled"
     fi
 
-    if [ $breakfast_returncode -ne 0 ]; then
-        echo ">> [$(date)] breakfast failed for $codename, $branch branch" | tee -a "$DEBUG_LOG"
-        # call post-build.sh so the failure is logged in a way that is more visible
+    if [ $lunch_returncode -ne 0 ]; then
+        echo ">> [$(date)] lunch failed for $codename, $branch branch" | tee -a "$DEBUG_LOG"
         if [ -f /root/userscripts/post-build.sh ]; then
           echo ">> [$(date)] Running post-build.sh for $codename" >> "$DEBUG_LOG"
           /root/userscripts/post-build.sh "$codename" false "$branch" &>> "$DEBUG_LOG" || echo ">> [$(date)] Warning: post-build.sh failed!"
@@ -422,7 +452,6 @@ for codename in ${devices//,/ }; do
     fi
 
     # Apply the PlayIntegrity patch if the APPLY_PI_PATCH is set
-    # but not for `gta4l` & gta4lwifi
     if [ "$APPLY_PI_PATCH" = true ] && [ "$codename" != gta4l ] && [ "$codename" != gta4lwifi ]; then
       echo ">> [$(date)] Applying the PlayIntegrity patch for $codename" >> "$DEBUG_LOG"
       cd system/core
@@ -447,40 +476,44 @@ for codename in ${devices//,/ }; do
     # Call mka
     build_successful=true
     if [ "$CALL_MKA" = true ]; then
-      # Start the build
       echo ">> [$(date)] Starting build for $codename, $branch branch" | tee -a "$DEBUG_LOG"
       build_successful=false
       files_to_hash=()
 
-      if (set +eu ; mka "${jobs_arg[@]}" target-files-package bacon) &>> "$DEBUG_LOG"; then
+      build_rc=0
+      set +eu
+      mka "${jobs_arg[@]}" target-files-package bacon &>> "$DEBUG_LOG"
+      build_rc=$?
+      set -eu
+
+      if [ $build_rc -eq 0 ]; then
         echo ">> [$(date)] Moving build artifacts for $codename to '$ZIP_DIR/$zipsubdir'" | tee -a "$DEBUG_LOG"
 
-        # Move the ROM zip files to the main OUT directory
         cd out/target/product/"$codename"
         files_to_hash=()
-        for build in lineage-*.zip; do
-          cp -v system/build.prop "$ZIP_DIR/$zipsubdir/$build.prop" &>> "$DEBUG_LOG"
+        for build in $(ls -1t *.zip 2>/dev/null | grep -Ev "(target_files|ota|symbol|img|fastboot)" || true); do
+          if [ -f system/build.prop ]; then
+            cp -v system/build.prop "$ZIP_DIR/$zipsubdir/$build.prop" &>> "$DEBUG_LOG"
+          fi
           mv "$build" "$ZIP_DIR/$zipsubdir/" &>> "$DEBUG_LOG"
           files_to_hash+=( "$build" )
         done
 
-        # Now handle the .img files - where are they?
         img_dir=$(find "$source_dir/out/target/product/$codename/obj/PACKAGING" -name "IMAGES")
         if [ -d "$img_dir" ]; then
           cd "$img_dir"
         fi
 
-        # rename and copy the images to the zips directory
         for image in recovery boot vendor_boot dtbo super_empty vbmeta vendor_kernel_boot init_boot; do
           if [ -f "$image.img" ]; then
-            recovery_name="lineage-$los_ver-$builddate-$RELEASE_TYPE-$codename-$image.img"
+            rom_prefix=${ROM_NAME_PREFIX:-$vendor}
+            recovery_name="${rom_prefix}-$los_ver-$builddate-$RELEASE_TYPE-$codename-$image.img"
             echo ">> [$(date)] Copying $image.img" to "$ZIP_DIR/$zipsubdir/$recovery_name" >> "$DEBUG_LOG"
             cp "$image.img" "$ZIP_DIR/$zipsubdir/$recovery_name" &>> "$DEBUG_LOG"
             files_to_hash+=( "$recovery_name" )
           fi
         done
 
-        # create the checksum files
         cd "$ZIP_DIR/$zipsubdir"
         for f in "${files_to_hash[@]}"; do
           sha256sum "$f" > "$ZIP_DIR/$zipsubdir/$f.sha256sum"
@@ -491,7 +524,6 @@ for codename in ${devices//,/ }; do
         echo ">> [$(date)] Failed build for $codename" | tee -a "$DEBUG_LOG"
       fi
 
-      # Remove old zips and logs
       if [ "$DELETE_OLD_ZIPS" -gt "0" ]; then
         if [ "$ZIP_SUBDIR" = true ]; then
           /usr/bin/python /root/clean_up.py -n "$DELETE_OLD_ZIPS" -V "$los_ver" -N 1 "$ZIP_DIR/$zipsubdir"
@@ -510,7 +542,6 @@ for codename in ${devices//,/ }; do
       echo ">> [$(date)] Calling mka for $codename, $branch branch disabled"
     fi
 
-    # call post-build.sh
     if [ -f /root/userscripts/post-build.sh ]; then
       echo ">> [$(date)] Running post-build.sh for $codename" >> "$DEBUG_LOG"
       /root/userscripts/post-build.sh "$codename" "$build_successful" "$branch" &>> "$DEBUG_LOG" || {
@@ -527,7 +558,6 @@ for codename in ${devices//,/ }; do
 done
 
 if [ "$CLEAN_AFTER_BUILD" = true ]; then
-    # Now we can cleanup vendor/partner_gms)
     echo ">> [$(date)] Removing $PWD/vendor/partner_gms" | tee -a "$DEBUG_LOG"
     rm -rf vendor/partner_gms || true
 fi
